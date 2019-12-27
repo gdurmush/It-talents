d8cb147865fcce0f310317215f42ab157f178167
yanislavvejdarski <yanislav.vejdarski@gmail.com>
2019-12-27 21:33:08 +0200

    Add Some Interface

---
+++ Controller/favouriteController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Controller;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include_once dirname(__FILE__) . "/../Model/DBManager.php";
+class FavouriteController{
+    public function show(){
+
+    }
+}
--- View/main.php
+++ View/main.php
@@ -1,3 +1,9 @@
+
+<a href="index.php?target=cart&action=show"><button>Cart</button></a>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <?php
 
 echo "You are in main";
+include_once "View/search.php";
+
+?>
